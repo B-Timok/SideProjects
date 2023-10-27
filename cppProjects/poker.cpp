cf7ffce6f26290f5c29f270d50c6d65d3e235790
Brandon <timok@unlv.nevada.edu>
2023-10-27 01:06:32 -0700

    Initial commit of SideProjects

---
+++ cppProjects/poker.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "\nThis is a program to calculate Pre-Flop hand equities in Texas Holdem." << std::endl;
+    std::cout << '\n';
+    
+    return 0;
+}
