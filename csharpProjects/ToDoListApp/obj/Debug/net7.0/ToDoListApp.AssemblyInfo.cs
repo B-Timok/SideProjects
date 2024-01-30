cfd8d0e82fe780f85a6b1ffc11c0f0fe45a36736
Brandon Timok <99372902+B-Timok@users.noreply.github.com>
2024-01-30 22:53:57 +0000

    condensed all tasks into one script file

--- csharpProjects/ToDoListApp/obj/Debug/net7.0/ToDoListApp.AssemblyInfo.cs
+++ csharpProjects/ToDoListApp/obj/Debug/net7.0/ToDoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ffec3956ae0c621a96f0670e6ef3f934b27582d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686ac80209588e67e56b10bd8c68c07ee3cb20e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
