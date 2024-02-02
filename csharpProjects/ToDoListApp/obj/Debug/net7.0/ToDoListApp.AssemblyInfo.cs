012881691480c2bd1bfb50594ad8998fda97e86e
Brandon Timok <99372902+B-Timok@users.noreply.github.com>
2024-02-02 21:12:10 +0000

    created directory for ohhshoot website

--- csharpProjects/ToDoListApp/obj/Debug/net7.0/ToDoListApp.AssemblyInfo.cs
+++ csharpProjects/ToDoListApp/obj/Debug/net7.0/ToDoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686ac80209588e67e56b10bd8c68c07ee3cb20e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867bbc5fa8b1b8db4c9f3800cf7b12af6bdb3549")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
