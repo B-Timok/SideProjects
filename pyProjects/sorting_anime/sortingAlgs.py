fee41b50fa5d274d2346fd84a983135e3cd9bf33
Brandon Timok <99372902+B-Timok@users.noreply.github.com>
2024-03-08 10:31:17 -0800

    Update sortingAlgs.py
    removed some unnecessary lines

--- pyProjects/sorting_anime/sortingAlgs.py
+++ pyProjects/sorting_anime/sortingAlgs.py
@@ -89,25 +89,24 @@ def merge_sort(A, screen):
                 A[k] = right_half[j]
                 j += 1
             k += 1
-            draw_bars(A, screen)  # Draw the bars after each merge step
+            draw_bars(A, screen)
 
         while i < len(left_half):
             A[k] = left_half[i]
             i += 1
             k += 1
-            draw_bars(A, screen)  # Draw the bars after each merge step
+            draw_bars(A, screen)
 
         while j < len(right_half):
             A[k] = right_half[j]
             j += 1
             k += 1
-            draw_bars(A, screen)  # Draw the bars after each merge step
+            draw_bars(A, screen)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     
-    # Set the title of the window depending on the algorithm
     pygame.display.set_caption("Sorting Animation")
 
     A = generate_random_list()
