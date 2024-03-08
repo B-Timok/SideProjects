8155ba38d9de3f65a5e4670d0f0b186c15f8881d
Brandon Timok <99372902+B-Timok@users.noreply.github.com>
2024-03-08 18:24:26 +0000

    Finished adding pygame project to web portfolio in sideprojects

--- csharpProjects/ToDoListApp/obj/Debug/net7.0/ToDoListApp.AssemblyInfo.cs
+++ csharpProjects/ToDoListApp/obj/Debug/net7.0/ToDoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c137cae18bc95736e4923e6feaae2d04ad9ea0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8fb57149555cb34371749507447fbd1a0cfd1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
